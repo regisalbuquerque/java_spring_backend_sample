48f296b01dd35b815e3f3d2320acab4f068aeb36
Regis Albuquerque <regis.albuquerque1@gmail.com>
2019-10-16 14:16:25 -0500

    Adicionando Edicao e Exclusao de categorias. E o botao novo.

--- project/src/main/java/br/sample/project/controller/CategoriasController.java
+++ project/src/main/java/br/sample/project/controller/CategoriasController.java
@@ -3,21 +3,60 @@ package br.sample.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.sample.project.model.Categoria;
 import br.sample.project.repository.CategoriasRepository;
 
 @Controller
+@RequestMapping("/categorias")
 public class CategoriasController {
 	
 	@Autowired
 	private CategoriasRepository categoriasRepository;
 	
-	@GetMapping("/categorias")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaCategorias");
 		modelAndView.addObject("categorias", categoriasRepository.findAll());
+		modelAndView.addObject(new Categoria());
 		return modelAndView;
+	}
+	
+	@GetMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView("ListaCategorias");
+		modelAndView.addObject("categorias", categoriasRepository.findAll());
+		modelAndView.addObject(new Categoria());
+		return modelAndView;
+	}
+	
+	@GetMapping("/{id}/editar")
+	public ModelAndView editar(@PathVariable Integer id) {
+		ModelAndView modelAndView = new ModelAndView("ListaCategorias");
+		modelAndView.addObject("categorias", categoriasRepository.findAll());
+		modelAndView.addObject("categoria", categoriasRepository.findById(id.longValue()));
+		return modelAndView;
+	}
+	
+	@GetMapping("/{id}/excluir")
+	public ModelAndView excluir(@PathVariable Integer id) {
+		ModelAndView modelAndView = new ModelAndView("ListaCategorias");
+		Categoria categoria = categoriasRepository.findById(id.longValue()).get();
+		categoriasRepository.delete(categoria);
+		
+		modelAndView.addObject("categorias", categoriasRepository.findAll());
+		modelAndView.addObject(new Categoria());
+		return modelAndView;
+	}
+	
+	@PostMapping
+	public String salvar(Categoria categoria) {
+		categoriasRepository.save(categoria);
+		return "redirect:/categorias";
 	}
 
 }
