50f32a72112e788ddc8b5eb76b8ed8bb355c4109
Regis Albuquerque <regis.albuquerque1@gmail.com>
2019-10-16 10:28:26 -0500

    Adicionando a lista de categorias na view e dados de teste no arquivo import.sql

---
+++ project/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into categoria (id, nome, descricao) values (1, 'Informática','Computadores e Acessórios'); 
+insert into categoria (id, nome, descricao) values (2, 'Livros', 'Livros em geral'); 
+insert into categoria (id, nome, descricao) values (3, 'Esporte', 'Esporte e Lazer');
